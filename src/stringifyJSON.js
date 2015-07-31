beca60b4a7590d9608f7312a14288a522144b702
xerxesv <xerxesv@gmail.com>
2015-07-30 17:51:37 -0700

    strange bug in stringifyJSON when stringifying empty objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === "number" || typeof obj === "boolean"){
+	  console.log(String(obj));
+	  return String(obj);
+  }
+  else if (typeof obj === "string"){
+	  console.log('"'+obj+'"');
+	  return '"' + obj + '"';
+  }
+  else if (typeof obj === "object"){
+	  if (obj === null) return "null";
+	  else if (Array.isArray(obj)){
+		  var arrStr = "[";
+		  obj.forEach(function(element,index,arr){
+			 arrStr += stringifyJSON(element);
+			 if(index < arr.length - 1){
+				 arrStr += ",";
+			 }
+		  });
+		  arrStr += "]";
+		  console.log(arrStr);
+		  return arrStr;
+	  }
+	  else {
+		  console.dir(obj);
+		  var validKeys = Object.keys(obj).filter(function(key){
+			 return key !== undefined && typeof key !== "function" 
+		  });
+		  var objStr = "{";
+		  
+		  validKeys.forEach(function(key,index,keys){
+				  objStr = objStr + stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
+				  if(index < keys.length - 1) objStr += ",";
+			});
+		  objStr += "}";
+		  console.log(objStr);
+		  return objStr;
+	  } 
+  }
+  else {
+	  console.log("invalid");
+	  return stringifyJSON(null);
+  }
 };
